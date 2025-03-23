f29790f7d5ee96b6249a33a9959d2b2d73f846c9
Rahal Jayathilaka <rahaljayathilaka@Mac.ht.home>
2025-03-23 03:26:40 -0400

    Build working WAR for Render deployment

--- src/main/java/com/sharemeals/SharemealsApplication.java
+++ src/main/java/com/sharemeals/SharemealsApplication.java
@@ -2,13 +2,17 @@ package com.sharemeals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SharemealsApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SharemealsApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SharemealsApplication.class, args);
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SharemealsApplication.class);
+    }
 }
